85def09c11ee98cb160d53b9e87e8b127593b4cf
LeoNaab <102571504+LeoNaab@users.noreply.github.com>
2023-10-13 10:48:37 -0700

    Update main.cpp

--- Imagination_board_c/src/main.cpp
+++ Imagination_board_c/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
-
+const int LED = 14;
+int i = 0;
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED, OUTPUT);
+  USBSerial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(i<5){
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    i++;
+  }
+  USBSerial.println("Hello, World! make way for the MTN climbers JK");
+  delay(1000);
+  // put your main code here, to run five times:
 }
